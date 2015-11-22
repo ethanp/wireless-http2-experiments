65e13ad404a128f13e75ed7b0f3f38ca19bad314
ethanp <ethanp@utexas.edu>
2015-11-21 22:54:51 -0600

    joint http 1 & 2 server is working

--- java-server-side/src/main/java/ethanp/examples/Http2EchoServer.java
+++ java-server-side/src/main/java/ethanp/examples/Http2EchoServer.java
@@ -2,9 +2,10 @@ package ethanp.examples;
 
 import org.apache.commons.io.IOUtils;
 import org.eclipse.jetty.alpn.ALPN;
+import org.eclipse.jetty.alpn.server.ALPNServerConnectionFactory;
 import org.eclipse.jetty.http.HttpVersion;
+import org.eclipse.jetty.http2.HTTP2Cipher;
 import org.eclipse.jetty.http2.server.HTTP2ServerConnectionFactory;
-import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.HttpConfiguration;
 import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.NegotiatingServerConnectionFactory;
@@ -40,6 +41,8 @@ public class Http2EchoServer {
                 case "GET":
                     if (request.getServletPath().contains("/data")) {
 
+                        // THIS COMMENT BROUGHT TO YOU BY "FSTAB" HIMSELF
+                        //
                         // This request does not necessarily come from the client.
                         // It may be triggered by the PUSH_PROMISE.
                         //
@@ -106,16 +109,12 @@ public class Http2EchoServer {
         http_config.setSecurePort(8443);
         http_config.setOutputBufferSize(32768);
 
-        /* make the regular (unencrypted) HTTP server connector */
-        ServerConnector http1Connector = new ServerConnector(server, new HttpConnectionFactory(http_config));
-        http1Connector.setPort(8080);
-        http1Connector.setIdleTimeout(30000);
-
         // SSL Context Factory for HTTPS and HTTP/2
         SslContextFactory sslContextFactory = new SslContextFactory();
         sslContextFactory.setKeyStorePath(HttpsServer.keystoreFile.getAbsolutePath());
         sslContextFactory.setKeyStorePassword(HttpsServer.PASSWORD);
         sslContextFactory.setKeyManagerPassword(HttpsServer.PASSWORD);
+        sslContextFactory.setCipherComparator(HTTP2Cipher.COMPARATOR);
 
         // HTTPS Configuration
         HttpConfiguration httpsConfig = new HttpConfiguration(http_config);
@@ -127,21 +126,26 @@ public class Http2EchoServer {
         // makes program bomb out if ALPN is not available in boot classpath
         NegotiatingServerConnectionFactory.checkProtocolNegotiationAvailable();
 
+        ALPNServerConnectionFactory alpn = new ALPNServerConnectionFactory();
+        alpn.setDefaultProtocol("h2");
+
         // SSL Connection Factory
         SslConnectionFactory sslFactory = new SslConnectionFactory(
             sslContextFactory,
-            HttpVersion.HTTP_2.asString()
+            alpn.getProtocol()
         );
 
         HttpConnectionFactory httpFactory = new HttpConnectionFactory(httpsConfig);
 
         // HTTP/2 Connector (no idea if this is right)
-        ServerConnector https2Connector = new ServerConnector(server,
-            sslFactory
-            , http2Factory
-//           , httpFactory // I think this isn't here by default because http2 is there.
+        ServerConnector https2Connector = new ServerConnector(
+            server,
+            sslFactory,
+            alpn,
+            http2Factory
         );
         https2Connector.setPort(8443);
+        https2Connector.setIdleTimeout(500000);
 
         /* Now we can create the HTTPS ServerConnector */
         ServerConnector https1Connector = new ServerConnector(
@@ -158,9 +162,10 @@ public class Http2EchoServer {
         /* Now requests can flow into the server from both HTTP and HTTPS URLs to
          * their respective ports and be processed accordingly by Jetty.
          */
-        server.setConnectors(new Connector[]{http1Connector, https2Connector, https1Connector});
+        server.addConnector(https2Connector);
+        server.addConnector(https1Connector);
 
-        ALPN.debug = true; // wtf?
+        ALPN.debug = true; // not sure what the output here means.
 
         server.start();
         server.join();
