ee18158f88528551271f881b5f954feb3a2b79ae
ethanp <ethanp@utexas.edu>
2015-11-24 18:05:08 -0600

    delete trials troubles and tribulations

--- swift-client-v2/swift-client-v2/ViewController.swift
+++ swift-client-v2/swift-client-v2/ViewController.swift
@@ -17,74 +17,27 @@ let BASE_PORT = 12345
 class ViewController: UIViewController {
 
     // MARK: Lifecycle
-    // Do any additional setup after loading the view
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.progressBar.setProgress(0.0, animated: false)
     }
 
     // MARK: UI Elements
-    @IBOutlet weak var simpleRequestButton: UIButton!
-    @IBOutlet weak var timeTcpButton: UIButton!
-    @IBOutlet weak var fiveTcpsButton: UIButton!
     @IBOutlet weak var fireRepeatedly: UIButton!
     @IBOutlet weak var fiveRepeatedly: UIButton!
     
     @IBOutlet weak var http1FlurryButton: UIButton!
-    @IBOutlet weak var http2FlurryButton: UIButton!
     
     @IBOutlet weak var debugTextArea: UITextView!
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var webView: UIWebView!
-    
-    // MARK: Attributes
-    lazy var dateFormatter: NSDateFormatter = {
-        let dateFormatter = NSDateFormatter()
-        dateFormatter.dateFormat = "HH:mm:ss:SSS"
-        return dateFormatter
-    }()
-    
-    var buttons : [UIButton?] {
-        get {
-            return [
-                simpleRequestButton,
-                timeTcpButton,
-                fiveTcpsButton,
-                fireRepeatedly,
-                fiveRepeatedly
-            ]
-        }
-    }
-
-    var singleTcpBenchmarker: TcpBenchmarker?
-    var fiveTcpBenchmarker: TcpBenchmarker?
 
     // MARK: Button Responses
-    @IBAction func simpleRequestPressed(sender: UIButton) {
-        sampleGET()
-    }
-
-    /** this just does ONE tcp connection */
-    @IBAction func timeTcpPressed(sender: UIButton) {
-        singleTcpBenchmarker = TcpBenchmarker(syncCount: 1, bytesToDwnld: 6)
-        singleTcpBenchmarker!.collectAndUploadResults()
-    }
-
-    @IBAction func time5TcpsPressed(sender: UIButton) {
-        fiveTcpBenchmarker = TcpBenchmarker(syncCount: 5, bytesToDwnld: 6)
-        fiveTcpBenchmarker!.collectAndUploadResults()
-    }
-
     @IBAction func fireRepeatedly(sender: UIButton)     { exploreTheSpace(1) }
     @IBAction func fiveConnRepeatedly(sender: UIButton) { exploreTheSpace(5) }
-
-    func setButtonsEnabled(bool: Bool) {
-        for b in buttons {
-            b?.enabled = bool
-        }
-    }
-    
-    override func viewDidLayoutSubviews() {
-        self.progressBar.setProgress(0.0, animated: false)
+    @IBAction func runHttpExperiment(sender: UIButton) {
+        HttpBenchmarker(vc: self)
+            .collectAndUploadResults()
     }
     
     // just used in exploreTheSpace, maybe could be moved into that method
@@ -124,56 +77,8 @@ class ViewController: UIViewController {
             }
         }
     }
-
-
-    // MARK: Example Implementations
-    func sampleGET() {
-        Alamofire.request(.GET, "http://httpbin.org/get", parameters: ["foo": "bar"])
-            .responseJSON { res in
-                print(res.request)  // original URL request
-                print(res.response) // URL response
-                print(res.data)     // server data
-                print(res.result)   // result of response serialization
-
-                if let json = res.result.value {
-                    print("JSON: \(json)")
-                }
-        }
-    }
-
-    func jsonDictExample() -> [String:AnyObject] {
-        let d2 = ["asdf": 1234, "bsdf": 2345]
-        let d1 = ["title": "TCP Vary", "start": dateString(), "cons": d2]
-        return d1 as! [String : AnyObject]
-    }
-
-    func uploadData(data: [String:AnyObject]) {
-        //        let parameters = [
-        //            "foo": [1,2,3],
-        //            "bar": [
-        //                "baz": "qux"
-        //            ]
-        //        ]
-
-        print("uploading")
-
-        Alamofire.request(
-            .POST,
-            "http://localhost:4567/data",
-            //            parameters: parameters,
-            parameters: data,
-            encoding: .JSON
-            ).responseJSON { response in
-                debugPrint(response)
-        }
-        // HTTP body: {"foo": [1, 2, 3], "bar": {"baz": "qux"}}
-    }
-
-
+    
     // MARK: Utilities
-    func dateString() -> String {
-        return self.dateFormatter.stringFromDate(NSDate())
-    }
     
     func displayText(text: AnyObject) {
         print(text)
@@ -181,28 +86,5 @@ class ViewController: UIViewController {
             self.debugTextArea.text = "\(text)"
         }
     }
-    
-    //////////////////////////////////////////////////
-    // THIS IS WHERE THE HTTP EXPERIMENT CODE LIVES //
-    //////////////////////////////////////////////////
-    
-    @IBAction func http1FlurryPressed(sender: UIButton) {
-//        HttpBenchmarker(version: .ONE, trials: 2)
-//            .collectAndUploadResults()
-        displayURL()
-    }
-    
-    
-    func displayURL() {
-        HttpBenchmarker(vc: self)
-            .collectAndUploadResults()
-    }
-    
-    @IBAction func http2FlurryPressed(sender: UIButton) {
-        HttpBenchmarker(vc: self)
-            .collectAndUploadResults()
-    }
-    
-
 }
 
